5e593b1446fcfac51ce5ebab1529fa91f4029a4d
Harshal Dave <harshaldave74@gmail.com>
2022-11-18 03:38:34 +0530

    :lock: route restrictions and auth

--- src/layouts/dashboard/header/AccountPopover.js
+++ src/layouts/dashboard/header/AccountPopover.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 // @mui
 import { alpha } from '@mui/material/styles';
 import { Box, Divider, Typography, Stack, MenuItem, Avatar, IconButton, Popover } from '@mui/material';
@@ -25,6 +26,7 @@ const MENU_OPTIONS = [
 // ----------------------------------------------------------------------
 
 export default function AccountPopover() {
+  const navigate = useNavigate();
   const [open, setOpen] = useState(null);
 
   const handleOpen = (event) => {
@@ -33,6 +35,12 @@ export default function AccountPopover() {
 
   const handleClose = () => {
     setOpen(null);
+  };
+
+  const handleLogout = () => {
+    setOpen(null);
+    navigate('/login', { replace: true });
+
   };
 
   return (
@@ -97,8 +105,8 @@ export default function AccountPopover() {
 
         <Divider sx={{ borderStyle: 'dashed' }} />
 
-        <MenuItem onClick={handleClose} sx={{ m: 1 }}>
-          Logout
+        <MenuItem onClick={handleLogout} sx={{ m: 1 }}>
+            Logout
         </MenuItem>
       </Popover>
     </>
--- src/sections/auth/login/LoginForm.js
+++ src/sections/auth/login/LoginForm.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-restricted-globals */
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // @mui
-import { Link, Stack, IconButton, InputAdornment, TextField, Checkbox } from '@mui/material';
+import { Link, Stack, IconButton, InputAdornment, TextField, Checkbox, Box } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 // components
 import Iconify from '../../../components/iconify';
@@ -10,21 +11,30 @@ import Iconify from '../../../components/iconify';
 
 export default function LoginForm() {
   const navigate = useNavigate();
-
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
   const handleClick = () => {
-    navigate('/dashboard', { replace: true });
+    console.log(`${email}   ${pwd}`);
+
+    if (email === "roads@gov.in" && pwd === "modiji") {
+      navigate('/dashboard', { replace: true });
+    } else {
+      alert('Invalid credentials');
+    }
   };
 
   return (
     <>
       <Stack spacing={3}>
-        <TextField name="email" label="Email address" />
+        <TextField name="email" label="Email address" onChange={(event) => setEmail(event.target.value)} value={email} />
 
         <TextField
           name="password"
           label="Password"
+          onChange={(event) => setPwd(event.target.value)}
+          value={pwd}
           type={showPassword ? 'text' : 'password'}
           InputProps={{
             endAdornment: (
@@ -39,7 +49,9 @@ export default function LoginForm() {
       </Stack>
 
       <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
-        <Checkbox name="remember" label="Remember me" />
+        <Box>
+          <Checkbox name="remember" label="Remember me" />Remember Me
+        </Box>
         <Link variant="subtitle2" underline="hover">
           Forgot password?
         </Link>
